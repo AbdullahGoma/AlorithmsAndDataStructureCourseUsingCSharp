4a9787d78c7213792b551de09e60e077845c3bfd
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2024-02-12 18:18:20 +0200

    Algorithms Analysis

---
+++ IntroToAlgorithmsAnalysis/Program.cs
@@ -0,0 +1,30 @@
+﻿using AlgorithmsAndDataStructLib;
+using System.Diagnostics;
+
+namespace IntroToAlgorithmsAnalysis
+{
+	internal partial class Program
+	{
+		static void Main(string[] args)
+		{
+			// 260 milliseconds for 1K
+			// 13 seconds for 4K
+			//  1.40  mins for 8k
+			// 800 seconds for 16k
+
+			var ints = In.ReadInts("8Kints.txt").ToArray();
+
+			var watch = new Stopwatch();
+			watch.Start();
+
+
+			var triplets = ThreeSum.Count(ints);
+
+		    watch.Stop();
+
+			Console.WriteLine($"The number of \"zero-some\" triplets: {triplets}");
+			Console.WriteLine($"Time taken: {watch.Elapsed}");
+			//Console.ReadLine();
+		}
+	}
+}
